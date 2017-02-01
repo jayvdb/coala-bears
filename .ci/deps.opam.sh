d3f4c14409f47ab194f467fe9232c80341df5c71
John Vandenberg <jayvdb@gmail.com>
2017-02-01 07:33:58 +0700

    WIP: Split tests by runtime

---
+++ .ci/deps.opam.sh
@@ -0,0 +1,14 @@
+set -e
+
+# Infer commands
+if [ ! -e ~/infer-linux64-v0.7.0/infer/bin ]; then
+  wget -nc -O ~/infer.tar.xz https://github.com/facebook/infer/releases/download/v0.7.0/infer-linux64-v0.7.0.tar.xz
+  tar xf ~/infer.tar.xz -C ~/
+  cd ~/infer-linux64-v0.7.0
+  opam init --y
+  opam update
+  opam pin add --yes --no-action infer .
+  opam install --deps-only --yes infer
+  ./build-infer.sh java
+  cd -
+fi
