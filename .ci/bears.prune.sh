bd183bc457534af690710362804374dd982d04eb
John Vandenberg <jayvdb@gmail.com>
2017-02-03 22:27:11 +0700

    Add apt-get

--- .ci/bears.prune.sh
+++ .ci/bears.prune.sh
@@ -20,6 +20,8 @@ elif [[ "${BEARS/node/}" != "$BEARS" ]]; then
   BEARS=npm
 elif [[ "$BEARS" == "r" ]]; then
   BEARS=rscript
+elif [[ "$BEARS" == "generic" ]]; then
+  BEARS=apt-get
 fi
 
 bears=$(find bears -type f -and -name '*Bear.py' | sort)
@@ -42,10 +44,12 @@ non_pip_runtime_requirement_bears="$cabal_requirement_bears $gem_requirement_bea
 
 other_requirement_bears=$(comm -23 <(ls $requirement_bears) <(ls $non_pip_runtime_requirement_bears $pip_requirement_bears))
 
-distribution_only_bears=$(grep -Pl 'DistributionRequirement\(' $other_requirement_bears)
+distribution_requirement_bears=$(grep -m 1 -l 'DistributionRequirement(' $requirement_bears)
+
+apt_get_requirement_bears=$(grep -m 1 -l 'apt_get' $distribution_requirement_bears)
 
 # Verify that DistributionRequirement is the only other Requirement subclass used
-unknown_requirement_bears=$(grep -Pl '(?<!Distribution)Requirement\(' $other_requirement_bears || true)
+unknown_requirement_bears=$(grep -m 1 -Pl '(?<!Distribution)Requirement\(' $other_requirement_bears || true)
 
 if [[ -n "$unknown_requirement_bears" ]]; then
   echo "Unknown requirements for $unknown_requirement_bears"
@@ -79,11 +83,28 @@ if [[ -n "$non_yield_result_other_bears" ]]; then
   exit 1
 fi
 
+echo Other exe $executable_linter_bears $executable_other_bears
+
 python_bears="$pip_only_requirement_bears $clang_bears $other_bears"
 
 non_python_bears=$(comm -23 <(ls $bears) <(ls $python_bears))
 
 cabal_requirement_bears="$cabal_requirement_bears bears/haskell/HaskellLintBear.py bears/shell/ShellCheckBear.py"
+
+dart_bears=$(ls bears/dart/*Bear.py)
+julia_bears=$(ls bears/julia/*Bear.py)
+php_bears=$(ls bears/php/*Bear.py)
+java_bears=$(grep -m 1 -l "'java'" $executable_linter_bears)
+pmd_bears=$(grep -m 1 -l 'PMD is missing' $executable_linter_bears)
+java_bears="$java_bears $pmd_bears"
+
+apt_get_requirement_bears=$(echo $apt_get_requirement_bears | xargs -n 1 | egrep -v '(Haskell|Julia|Lua)' )
+if [[ "$DIST" == "precise" ]]; then
+  apt_get_requirement_bears=$(echo $apt_get_requirement_bears | xargs -n 1 | grep -v 'PHPCodeSniffer' )
+fi
+if [[ "$DIST" == "debian-sid" ]]; then
+  apt_get_requirement_bears=$(echo $apt_get_requirement_bears | xargs -n 1 | egrep -v '(CSharp|PerlCritic|PHPCodeSniffer|PHPLintBear)' )
+fi
 
 remove_bears=''
 
@@ -94,14 +115,24 @@ if [[ $BEARS == "python" ]]; then
   remove_bears="$non_python_bears"
 elif [[ $BEARS == "cabal" ]]; then
   remove_bears=$(comm -23 <(ls $bears) <(ls $cabal_requirement_bears))
+elif [[ $BEARS == "dart" ]]; then
+  remove_bears=$(comm -23 <(ls $bears) <(ls $dart_bears))
 elif [[ $BEARS == "gem" ]]; then
   remove_bears=$(comm -23 <(ls $bears) <(ls $gem_requirement_bears))
 elif [[ $BEARS == "go" ]]; then
   remove_bears=$(comm -23 <(ls $bears) <(ls $go_requirement_bears))
+elif [[ $BEARS == "java" ]]; then
+  remove_bears=$(comm -23 <(ls $bears) <(ls $java_bears))
+elif [[ $BEARS == "julia" ]]; then
+  remove_bears=$(comm -23 <(ls $bears) <(ls $julia_bears))
+elif [[ $BEARS == "php" ]]; then
+  remove_bears=$(comm -23 <(ls $bears) <(ls $php_bears))
 elif [[ $BEARS == "npm" ]]; then
   remove_bears=$(comm -23 <(ls $bears) <(ls $npm_requirement_bears))
 elif [[ $BEARS == "rscript" ]]; then
   remove_bears=$(comm -23 <(ls $bears) <(ls $rscript_requirement_bears))
+elif [[ $BEARS == "apt-get" ]]; then
+  remove_bears=$(comm -23 <(ls $bears) <(ls $apt_get_requirement_bears))
 fi
 
 if [[ -n "$remove_bears" ]]; then
