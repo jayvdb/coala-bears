e324a6bc2b33b5263f9336a1c93f49a98374ed7a
John Vandenberg <jayvdb@gmail.com>
2017-02-03 20:54:37 +0700

    Add PHPLintBear

--- bears/php/PHPLintBear.py
+++ bears/php/PHPLintBear.py
@@ -13,6 +13,7 @@ class PHPLintBear:
     Checks the code with ``php -l``. This runs it on each file separately.
     """
     LANGUAGES = {'PHP'}
+    REQUIREMENTS = {DistributionRequirement(apt_get='php')}
     AUTHORS = {'The coala developers'}
     AUTHORS_EMAILS = {'coala-devel@googlegroups.com'}
     LICENSE = 'AGPL-3.0'
