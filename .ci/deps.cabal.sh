b33a4b5cb8265e7a3a5545fd9edf9db7aa14c776
John Vandenberg <jayvdb@gmail.com>
2017-02-04 11:46:43 +0700

    --force-reinstalls

--- .ci/deps.cabal.sh
+++ .ci/deps.cabal.sh
@@ -2,7 +2,6 @@ set -e
 set -x
 
 cabal update
-cabal install cabal-install-1.22.9.0
-cabal install --only-dependencies
+cabal install --force-reinstalls --only-dependencies
 
 $HOME/.ghc-mod/cabal-helper
