e5fa2c5a680ce770f184cc3f7862612e7fa23432
John Vandenberg <jayvdb@gmail.com>
2017-02-04 18:44:03 +0700

    Try again

--- .ci/deps.cabal.sh
+++ .ci/deps.cabal.sh
@@ -6,6 +6,10 @@ cabal update
 cat $HOME/.cabal/config
 find $HOME/.cabal
 
+
+echo "shadow-installed-packages: False" >> $HOME/.cabal/config
+echo "shared: " >> $HOME/.cabal/config
+
 ghc-pkg list
 
 cabal install cabal-install==1.22.9.0
