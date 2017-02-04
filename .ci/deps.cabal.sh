5bcb1007ab2b96af446e0f78f2c03d710ba17744
John Vandenberg <jayvdb@gmail.com>
2017-02-04 19:47:24 +0700

    Remove config after fetch

--- .ci/deps.cabal.sh
+++ .ci/deps.cabal.sh
@@ -21,18 +21,21 @@ function pkg_version_chk {
   fi
 }
 
-if [[ -n "$TRAVIS" ]]; then
-  # https://github.com/coala/coala-bears/issues/1380
-  if pkg_version_chk hlint 1.9.27 && pkg_version_chk ShellCheck 0.4.1 && pkg_version_chk ghc-mod 5.6.0.0 ; then
-    exit 0
-  fi
-
-  rm -rf $HOME/.cabal $HOME/.ghc $HOME/.ghc-mod
-fi
+#if [[ -n "$TRAVIS" ]]; then
+#  # https://github.com/coala/coala-bears/issues/1380
+#  if pkg_version_chk hlint 1.9.27 && pkg_version_chk ShellCheck 0.4.1 && pkg_version_chk ghc-mod 5.6.0.0 ; then
+#    exit 0
+#  fi
+#
+#  rm -rf $HOME/.cabal $HOME/.ghc $HOME/.ghc-mod
+#fi
 
 cabal update
 
 cabal install cabal-install==1.22.9.0
+
+rm $HOME/.cabal/config
+
 cabal install --only-dependencies --avoid-reinstalls
 
 # Force ghc-mod to resolve its Cabal version
