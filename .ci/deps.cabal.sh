6192faa57d3863a2aa9fb58b472ddfb1a099334d
John Vandenberg <jayvdb@gmail.com>
2017-02-04 12:43:26 +0700

    Add --force-reinstalls

--- .ci/deps.cabal.sh
+++ .ci/deps.cabal.sh
@@ -3,7 +3,7 @@ set -x
 
 cabal update
 #cabal install cabal-install==1.22.9.0
-cabal install --only-dependencies
+cabal install --only-dependencies --force-reinstalls
 
 # Force ghc-mod to resolve its Cabal version
 ~/.cabal/bin/ghc-mod modules
