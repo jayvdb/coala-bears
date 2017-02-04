a55815b3478b6b4de55ed10a0484f19ea8d34c2e
John Vandenberg <jayvdb@gmail.com>
2017-02-04 18:14:19 +0700

    Use cabal install 1.22.9.0

--- .ci/deps.cabal.sh
+++ .ci/deps.cabal.sh
@@ -8,7 +8,7 @@ find $HOME/.cabal
 
 ghc-pkg list
 
-# cabal install cabal-install==1.22.9.0
+cabal install cabal-install==1.22.9.0
 cabal install --only-dependencies --avoid-reinstalls
 
 # Force ghc-mod to resolve its Cabal version
