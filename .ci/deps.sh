68bee7193e473e0de9b6fe58530a7f141aea7e8c
John Vandenberg <jayvdb@gmail.com>
2017-05-25 13:06:58 +0700

    .ci/deps.sh: Build infer from git

--- .ci/deps.sh
+++ .ci/deps.sh
@@ -149,15 +149,12 @@ julia -e "Pkg.add(\"Lint\")"
 sudo luarocks install luacheck --deps-mode=none
 
 # Infer commands
-if [ ! -e ~/infer-linux64-v0.7.0/infer/bin ]; then
-  wget -nc -O ~/infer.tar.xz https://github.com/facebook/infer/releases/download/v0.7.0/infer-linux64-v0.7.0.tar.xz
-  tar xf ~/infer.tar.xz -C ~/
-  cd ~/infer-linux64-v0.7.0
-  opam init --y
-  opam update
-  opam pin add --yes --no-action infer .
-  opam install --deps-only --yes infer
+if [ ! -e ~/infer/bin ]; then
+  git clone https://github.com/facebook/infer.git
+  cd infer
+  # Compile Infer
   ./build-infer.sh java
+  # Install Infer into your PATH
 fi
 
 # PMD commands
