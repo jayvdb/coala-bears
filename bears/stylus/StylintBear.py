c0fda170b1a4368fa706e5ebac1e7b8f46b940de
John Vandenberg <jayvdb@gmail.com>
2018-07-04 10:46:58 +0700

    StylintBear: Line -> Column

--- bears/stylus/StylintBear.py
+++ bears/stylus/StylintBear.py
@@ -9,7 +9,7 @@ _setting_map = {True: 'always',
 
 
 @linter(executable='stylint',
-        normalize_line_numbers=True,
+        normalize_column_numbers=True,
         output_format='regex',
         output_regex=r'(?P<line>\d+):?(?P<column>\d+)?\s+.*?'
                      r'(?P<severity>error|warning)\s+(?P<message>.+?)'
