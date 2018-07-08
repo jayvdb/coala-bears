000fc3499c78915c12c3ed5f804b96c68a291538
John Vandenberg <jayvdb@gmail.com>
2018-07-08 21:01:22 +0700

    CI: Use cs50/ppa for astyle 3.0.1

--- .ci/deps.sh
+++ .ci/deps.sh
@@ -54,9 +54,3 @@ if [ ! -e ~/.local/bin/phpmd ]; then
   curl -fsSL -o  ~/.local/bin/phpmd http://static.phpmd.org/php/latest/phpmd.phar
   chmod +x ~/.local/bin/phpmd
 fi
-
-# astyle installation
-wget "https://downloads.sourceforge.net/project/astyle/astyle/astyle%203.0.1/astyle_3.0.1_linux.tar.gz?r=&ts=1499017588&use_mirror=excellmedia" -O ~/astyle.tar.gz
-tar -xvzf ~/astyle.tar.gz -C ~/
-make -C ~/astyle/build/gcc
-sudo make install -C ~/astyle/build/gcc
