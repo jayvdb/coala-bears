85c643bb2eda8588206322a2ae022d8b4e3cdf56
John Vandenberg <jayvdb@gmail.com>
2018-07-24 00:03:25 +0700

    Force coala dev version

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ import platform
 import sys
 from subprocess import call
 
+from distutils.version import LooseVersion
 import setuptools.command.build_py
 from setuptools import find_packages, setup
 from setuptools.command.test import test as TestCommand
@@ -100,6 +101,184 @@ SETUP_COMMANDS['docs'] = BuildDocsCommand
 __dir__ = os.path.dirname(__file__)
 
 
+class PEP440Version(LooseVersion):
+    """
+    Basic PEP440 version with a few features.
+
+    Uses the same version semantics as LooseVersion,
+    with the addition that a ``v`` prefix is allowed
+    in the version as required by PEP 440.
+
+    vstring may be a list, tuple or string.
+
+    v_prefix is auto-detected by default.
+    Set to False to remove if present, or True to add if missing.
+    """
+
+    def __init__(self, vstring=None, v_prefix=None):
+        self._v_prefix = v_prefix
+        if isinstance(vstring, (list, tuple)):
+            if len(vstring) and vstring[0].startswith('v):
+               self.version = type(vstring)(
+                    [vstring[0][1:]] + vstring[1:])
+            else:
+                self.version = vstring
+
+            self._v_prefix = v_prefix
+            self.vstring = str(self)
+        else:
+            super(PEP440Version, self).__init__(vstring)
+
+        self._final = None
+        self._previous = None
+
+    def __str__(self):
+        return (('v' if self._v_prefix else '') +
+                '.'.join([str(i) for i in self.version]))
+
+    def __repr__(self):
+        return "%s('%s')" % (self.__class__.__name__, str(self))
+
+    def parse(self, vstring):
+        if vstring.startswith('v'):
+            vstring = vstring[1:]
+            if self._v_prefix is not False:
+                self._v_prefix = True
+        super(PEP440Version, self).parse(vstring)
+        if self._v_prefix:
+            self.vstring = 'v' + self.vstring
+
+    @property
+    def is_dev(self):
+        return any(part == 'dev' for part in self.version)
+
+    @property
+    def final(self):
+        """
+        Provide only the final component of the version.
+
+        A new instance is return if this instance is not final.
+        """
+        if self._final is not None:
+            return self._final
+
+        for i, part in enumerate(self.version):
+            if not isinstance(part, int):
+                final = self.version[:i]
+                break
+        else:
+            self._final = self
+            return self
+
+        self._final = PEP440Version(final, self._v_prefix)
+
+        return self._final
+
+    @property
+    def is_final(self):
+        return self.final == self
+
+    @property
+    def is_zero(self):
+        return all(part == 0 for part in self.version)
+
+    def _estimate_previous(self):
+        """
+        Return a new version calculated to be the previous version.
+
+        Currently only handles when the current instance is a final version.
+
+        To really get the previous for 1.0.0, we need to consult PyPi,
+        git tags, or some other source of all released versions,
+        to find the highest patch release in the prior minor release, or
+        highest minor releases if there were no patch releases in the
+        last minor release, etc.
+
+        As a result, currently this assumes that release x.(x-1).0 exists
+        in that instance.
+        """
+        assert self.is_final, '%r is not final' % self
+
+        if self._previous:
+            return self._previous
+
+        if self.is_zero:
+            raise ValueError('version 0.0 detected; can not be decremented')
+
+        version = self.version
+        pos = len(version) -1
+
+        # Look for non-zero part
+        while pos != 0 and not version[pos]:
+            pos -= 1
+
+        previous = []
+        if pos:
+            previous = version[:pos]
+
+        previous += (version[pos] - 1, )
+
+        if pos <= len(version):
+            previous += [0] * (len(version) - pos - 2)
+
+            if len(previous) < len(version):
+                previous += (0, )
+
+        self._previous = PEP440Version(previous, self._v_prefix)
+        return self._previous
+
+
+def egg_name_to_requirement(name):
+    name = name.strip()
+    parts = name.split('-')
+
+    # The first part may be v or v0, which would be considered a version
+    # if processed in the following loop.
+    name_parts = [parts[0]]
+    # Pre-releases may contain a '-' and be alpha only, so we must
+    # parse from the second part to find the first version-like part.
+    for part in parts[1:]:
+        version = LooseVersion(part)
+        version = version.version
+        v_prefix = version[0] == 'v'
+        if v_prefix:
+            version = version[1:]
+        if isinstance(version[0], int):
+            break
+        name_parts.append(part)
+
+    version_parts = parts[len(name_parts):]
+
+    if not version_parts:
+        return name
+
+    name = '-'.join(name_parts)
+
+    version = PEP440Version('-'.join(version_parts))
+
+    # Assume that alpha, beta, pre, post & final releases
+    # are in PyPi so setutools can find it.
+    if not version.is_dev:
+        return name + '==' + version
+
+    # setuptools fails if a version is given with any specifier such as
+    # `==`, `=~`, `>`, if the version is not in PyPi.
+
+    # For development releases, which will not usually be PyPi,
+    # setuptools will typically fail.
+
+    # So we estimate a previous release that should exist in PyPi,
+    # by decrementing the lowest final version part, # and use version
+    # specifier `>` so that the installed package from VCS will have a
+    # version acceptable to the requirement.
+    previous_version = version.final._estimate_previous()
+
+    if previous_version.is_zero:
+        raise ValueError('Version %s could not be decremented' % version.version)
+
+    return name + '>' + str(previous_version)
+
+
 def read_requirements(filename):
     """
     Parse a requirements file.
@@ -125,7 +304,7 @@ def read_requirements(filename):
 
                 DEPENDENCY_LINKS.append(line)
 
-                line = egg_name.replace('-', '==')
+                line = egg_name_to_requirement(egg_name)
 
             data.append(line)
 
@@ -134,6 +313,8 @@ def read_requirements(filename):
 
 required = read_requirements('requirements.txt')
 required.remove('-r bear-requirements.txt')
+
+print(required)
 
 test_required = read_requirements('test-requirements.txt')
 
