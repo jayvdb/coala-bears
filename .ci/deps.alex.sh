541017572db2f10e400191c6add2b5c24fb0647c
John Vandenberg <jayvdb@gmail.com>
2019-05-18 12:06:47 +0700

    tox.ini: Create

---
+++ .ci/deps.alex.sh
@@ -0,0 +1,7 @@
+ALEX=$(which alex || true)
+# Delete 'alex' if it is not in a node_modules directory,
+# which means it is ghc-alex.
+if [[ -n "$ALEX" && "${ALEX/node_modules/}" == "${ALEX}" ]]; then
+  echo "Removing $ALEX"
+  sudo rm -rf $ALEX
+fi
