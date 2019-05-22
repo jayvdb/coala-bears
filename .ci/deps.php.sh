9dacd7ebd4866970fd51ab20beb002b06953e2c8
John Vandenberg <jayvdb@gmail.com>
2019-05-22 20:55:34 +0700

    Disable phpmd

--- .ci/deps.php.sh
+++ .ci/deps.php.sh
@@ -1,10 +1,10 @@
 # PHPMD installation
-if [ ! -e ~/.local/bin/phpmd ]; then
-  PHPMD='http://static.phpmd.org/php/latest/phpmd.phar'
-  mkdir -p ~/.local/bin
-  curl -fsSL -o  ~/.local/bin/phpmd "$PHPMD"
-  chmod +x ~/.local/bin/phpmd
-fi
+#if [ ! -e ~/.local/bin/phpmd ]; then
+#  PHPMD='http://static.phpmd.org/php/latest/phpmd.phar'
+#  mkdir -p ~/.local/bin
+#  curl -fsSL -o  ~/.local/bin/phpmd "$PHPMD"
+#  chmod +x ~/.local/bin/phpmd
+#fi
 
 pear install pear/PHP_CodeSniffer
 phpenv rehash
