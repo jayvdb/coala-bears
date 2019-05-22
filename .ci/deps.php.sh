b46cc1f33bdda0b4fd83a7be35b11ea923e393f9
John Vandenberg <jayvdb@gmail.com>
2019-05-22 11:23:04 +0700

    Split deps.sh

---
+++ .ci/deps.php.sh
@@ -0,0 +1,7 @@
+# PHPMD installation
+if [ ! -e ~/.local/bin/phpmd ]; then
+  PHPMD='http://static.phpmd.org/php/latest/phpmd.phar'
+  mkdir -p ~/.local/bin
+  curl -fsSL -o  ~/.local/bin/phpmd "$PHPMD"
+  chmod +x ~/.local/bin/phpmd
+fi
---
+++ .ci/deps.python.sh
@@ -0,0 +1,6 @@
+# Change environment for flawfinder from python to python2
+if [ ! -e ~/.local/bin/flawfinder ]; then
+  cp /usr/bin/flawfinder ~/.local/bin/flawfinder
+  sed -i '1s/.*/#!\/usr\/bin\/env python2/' ~/.local/bin/flawfinder
+  chmod +x ~/.local/bin/flawfinder
+fi
