b4d0b8229efbc360ad5d6b4a6c17efd889694dcb
John Vandenberg <jayvdb@gmail.com>
2019-05-23 17:19:53 +0700

    Force addition of Lint.jl v0.6.0

--- .ci/deps.julia.sh
+++ .ci/deps.julia.sh
@@ -5,4 +5,8 @@
 #mkdir deps
 #echo 'Pkg.add("Lint")' > deps/build.jl
 
-cat Manifest.toml || true
+#cat Manifest.toml || true
+
+# Normal package management not possible due to
+# https://github.com/tonyhffong/Lint.jl/issues/254
+julia -e 'using Pkg; Pkg.add(PackageSpec(url="https://github.com/tonyhffong/Lint.jl", rev="v0.6.0"))'
