78d59d39928cf76f270d0617dfb7c3eb550a97ac
John Vandenberg <jayvdb@gmail.com>
2019-05-23 15:54:42 +0700

    Julia and R

--- .ci/deps.r.sh
+++ .ci/deps.r.sh
@@ -3,9 +3,6 @@
 set -e
 set -x
 
-rm -rf $R_LIB_USER
-mkdir -p $R_LIB_USER
-
 # R commands
 echo '.libPaths( c( "'"$R_LIB_USER"'", .libPaths()) )' >> .Rprofile
 #echo 'options(repos=structure(c(CRAN="http://cran.rstudio.com")))' >> .Rprofile
