fcf67473258f6f2c99047d979145689b7c422138
John Vandenberg <jayvdb@gmail.com>
2019-05-23 15:18:22 +0700

    .ci/deps.r.sh: Clear cache

--- .ci/deps.r.sh
+++ .ci/deps.r.sh
@@ -3,6 +3,9 @@
 set -e
 set -x
 
+rm -rf $R_LIB_USER
+mkdir -p $R_LIB_USER
+
 # R commands
 echo '.libPaths( c( "'"$R_LIB_USER"'", .libPaths()) )' >> .Rprofile
 #echo 'options(repos=structure(c(CRAN="http://cran.rstudio.com")))' >> .Rprofile
