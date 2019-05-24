6c6eff0deae6b4f8a86227c0d29f11a760ee5701
John Vandenberg <jayvdb@gmail.com>
2019-05-24 10:31:52 +0700

    deps.ghc-mod.sh: Delete coala-bears.cabal

--- .ci/deps.generic.sh
+++ .ci/deps.generic.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 if [ -n "$BEARS" ]; then
-  for BEAR in $BEARS; do
-    if [ -f ".ci/deps.$BEARS.sh" ]; then
-      bash -e -x .ci/deps.$BEARS.sh
+  for BEAR in $BEARS $BEAR_LIST; do
+    if [ -f ".ci/deps.$BEAR.sh" ]; then
+      bash -e -x ".ci/deps.$BEAR.sh"
     fi
   done
 fi
