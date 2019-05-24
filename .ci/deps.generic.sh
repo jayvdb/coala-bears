271c95868377736485069e8995fb50435a511e09
John Vandenberg <jayvdb@gmail.com>
2019-05-24 08:01:05 +0700

    Fix adhoc

--- .ci/deps.generic.sh
+++ .ci/deps.generic.sh
@@ -1,5 +1,8 @@
+#!/bin/sh
 if [ -n "$BEARS" ]; then
-  if [ -f ".ci/deps.$BEARS.sh" ]; then
-    bash -e -x .ci/deps.$BEARS.sh
-  fi
+  for BEAR in $BEARS; do
+    if [ -f ".ci/deps.$BEARS.sh" ]; then
+      bash -e -x .ci/deps.$BEARS.sh
+    fi
+  done
 fi
