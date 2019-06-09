1ec32f612250fa2ca5ccf188ef9ae292ec2ebeb9
John Vandenberg <jayvdb@gmail.com>
2019-06-10 00:07:24 +0700

    Set PYTHON_ARCH for 32

--- .ci/AppVeyor.ps1
+++ .ci/AppVeyor.ps1
@@ -6,8 +6,12 @@ function Set-Python-Arch
   # run_with_env.cmd needs PYTHON_ARCH set to 64 for x64
   if ($env:Platform -eq 'x64')
   {
-    Set-ItemProperty -path 'HKCU:\Environment' -name 'PYTHON_ARCH' -value '64'
+    $PYTHON_ARCH = '64'
   }
+  else {
+    $PYTHON_ARCH = '32'
+  }
+  Set-ItemProperty -path 'HKCU:\Environment' -name 'PYTHON_ARCH' -value $PYTHON_ARCH
 }
 
 function Fix-AppVeyor
