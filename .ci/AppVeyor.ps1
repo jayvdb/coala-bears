9fc6831a0997fa59a02b850d71e1b2940ff5bbf1
John Vandenberg <jayvdb@gmail.com>
2019-06-10 21:52:31 +0700

    Re-compile gpy

--- .ci/AppVeyor.ps1
+++ .ci/AppVeyor.ps1
@@ -1,26 +1,21 @@
 . .ci/FudgeGenerateFake.ps1
 . .ci/PrepareAVVM.ps1
 
-function Set-Python-Arch
+function Rebuild-Gpy
 {
-  # run_with_env.cmd needs PYTHON_ARCH set to 64 for x64
-  if ($env:Platform -eq 'x64')
-  {
-    $PYTHON_ARCH = '64'
-  }
-  else {
-    $PYTHON_ARCH = '32'
-  }
-  Set-ItemProperty -path 'HKCU:\Environment' -name 'PYTHON_ARCH' -value $PYTHON_ARCH
+  $env:PATH = ('C:\Python27;' + $env:PATH)
+  node --version
+  $NODE_GPY_ROOT = "$env:NODE_ROOT\node_modules\npm\node_modules\node-gyp"
+  node "$NODE_GPY_ROOT\bin\node-gyp.js" "rebuild"
 }
 
 function Fix-AppVeyor
 {
-  Set-Python-Arch
-
   $config = Get-FudgefileContent .ci/Fudgefile.appveyor
 
   PackFakeNupkgs $config.packages
 
   Setup-Products $config.packages
+
+  Rebuild-Gpy
 }
