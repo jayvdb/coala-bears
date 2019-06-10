8e391b9af1ed4c08bb293b68d29a8a4a81817f65
John Vandenberg <jayvdb@gmail.com>
2019-06-11 00:21:07 +0700

    SpellCheckBearTest: Remove exclusion of Windows

--- tests/natural_language/SpellCheckBearTest.py
+++ tests/natural_language/SpellCheckBearTest.py
@@ -9,9 +9,6 @@ good_file = 'This is correct spelling.'
 bad_file = 'tihs si surly som incoreclt speling.'
 
 
-SpellCheckLintBearTest = unittest.skipIf(
-    platform.system() == 'Windows',
-    "SpellCheckBear doesn't work on windows")(
-        verify_local_bear(SpellCheckBear,
-                          valid_files=(good_file,),
-                          invalid_files=(bad_file,)))
+SpellCheckLintBearTest = verify_local_bear(SpellCheckBear,
+                                           valid_files=(good_file,),
+                                           invalid_files=(bad_file,))
