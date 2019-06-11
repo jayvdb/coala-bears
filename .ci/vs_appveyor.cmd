7809c78af7d8535cceb27a799c9e4f85abbb4a39
John Vandenberg <jayvdb@gmail.com>
2019-06-11 09:53:07 +0700

    .ci/vs_appveyor.cmd: Default to VS 2017

--- .ci/vs_appveyor.cmd
+++ .ci/vs_appveyor.cmd
@@ -4,18 +4,6 @@ ECHO %APPVEYOR_BUILD_WORKER_IMAGE% %PLATFORM%
 
 ECHO %VS2017_VC% %WIN71_SDK_ROOT% %VS14_VC%
 
-IF "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2017" (
-  ECHO Setting up VS2017 %PLATFORM%
-
-  IF %PLATFORM% == x64 (
-    call "%VS2017_VC%\Auxiliary\Build\vcvars64.bat"
-  )
-
-  IF %PLATFORM% == x86 (
-    call "%VS2017_VC%\Auxiliary\Build\vcvars32.bat"
-  )
-)
-
 IF "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2015" (
   ECHO Setting up VS2015 %PLATFORM%
 
@@ -28,3 +16,16 @@ IF "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2015" (
     call "%VS14_VC%\vcvarsall.bat" x86
   )
 )
+ELSE (
+  ECHO Setting up VS2017 %PLATFORM% for image %APPVEYOR_BUILD_WORKER_IMAGE%
+
+  IF %PLATFORM% == x64 (
+    call "%VS2017_VC%\Auxiliary\Build\vcvars64.bat"
+  )
+
+  IF %PLATFORM% == x86 (
+    call "%VS2017_VC%\Auxiliary\Build\vcvars32.bat"
+  )
+)
+
+
