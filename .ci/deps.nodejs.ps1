7c638819ec115964899855708fd45f727e030bac
John Vandenberg <jayvdb@gmail.com>
2019-06-14 19:29:16 +0700

    Add .ci/Fudgefile.travis

--- .ci/deps.nodejs.ps1
+++ .ci/deps.nodejs.ps1
@@ -10,19 +10,23 @@ function Configure-NPM
     npm config set msvs_version 2015
   }
 
-  # TODO: Split 'python' in Fudgefile into python27 and python3
+  # TODO: Use a python27 which is specified in the Fudgefile,
+  # either split 'python' in Fudgefile into python27 and python3,
+  # or use msys or cygwin python2.
+  $python27 = ''
   if (Test-Path C:\python27\python.exe)
   {
-    npm config set python C:\python27\python.exe
+    $python27 = "C:\python27\python.exe"
   }
-  elseif (Test-Path C:\msys64\usr\bin\python2.7.exe)
+  elseif (Test-Path "$env:ChocolateyInstall\bin\python.exe")
   {
-    $python27 = C:\msys64\usr\bin\python2.7.exe
+    $python = "$env:ChocolateyInstall\bin\python.exe"
   }
-  else
+  elseif (Test-Path C:\msys64\usr\bin/python2.7.exe)
   {
-    npm config set python $env:ChocolateyInstall\bin\python.exe
+    $python27 = "C:\msys64\usr\bin\python2.7.exe"
   }
+  npm config set python $python27
 }
 
 function Do-PostInstall
