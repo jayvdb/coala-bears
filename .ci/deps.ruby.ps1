701604103d651d963cb103637a87708daa0cd003
John Vandenberg <jayvdb@gmail.com>
2019-06-15 22:03:14 +0700

    Bundler

---
+++ .ci/deps.ruby.ps1
@@ -0,0 +1,13 @@
+Set-StrictMode -Version latest
+
+function Install-Bundler
+{
+  gem install bundler
+}
+
+function Do-PostInstall
+{
+  Install-Bundler
+}
+
+Export-ModuleMember -Function Config-NPM, Do-PostInstall
