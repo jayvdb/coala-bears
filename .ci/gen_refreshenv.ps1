76afc6916b57dae052f25cf0ae7415cd242084b5
John Vandenberg <jayvdb@gmail.com>
2019-06-16 16:29:15 +0700

    Default encoding isnt available

--- .ci/gen_refreshenv.ps1
+++ .ci/gen_refreshenv.ps1
@@ -5,4 +5,4 @@ Update-SessionEnvironment
 # Round brackets in variable names cause problems with bash
 Get-ChildItem env:* | Where-Object Name -cNotContains '(' | %{
   Write-Output ($_.Name + "='" + $_.Value + "'")
-} | Out-File -Encoding UTF8NoBOM C:\TEMP\refreshenv.sh
+} | Out-File -Encoding utf8 C:\TEMP\refreshenv.sh
