bc4df139e3ea59c5f06579be4b5637e4cf06fc7b
John Vandenberg <jayvdb@gmail.com>
2019-06-16 16:35:36 +0700

    Remove where

--- .ci/gen_refreshenv.ps1
+++ .ci/gen_refreshenv.ps1
@@ -3,6 +3,8 @@ Import-Module C:\ProgramData\chocolatey\helpers\chocolateyProfile.psm1
 Update-SessionEnvironment
 
 # Round brackets in variable names cause problems with bash
-Get-ChildItem env:* | Where-Object Name -cNotContains '(' | %{
-  Write-Output ($_.Name + "='" + $_.Value + "'")
+Get-ChildItem env:* | %{
+  if (!($_.Name -contains '(')) {
+    Write-Output ($_.Name + "='" + $_.Value + "'")
+  }
 } | Out-File -Encoding utf8 C:\TEMP\refreshenv.sh
