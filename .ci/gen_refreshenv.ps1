9bb00dc846a23565721d7fdb86f6267f52a7b9e3
John Vandenberg <jayvdb@gmail.com>
2019-06-16 17:27:40 +0700

    Contains

--- .ci/gen_refreshenv.ps1
+++ .ci/gen_refreshenv.ps1
@@ -4,7 +4,7 @@ Update-SessionEnvironment
 
 # Round brackets in variable names cause problems with bash
 Get-ChildItem env:* | %{
-  if (!($_.Name -contains '(')) {
+  if (!($_.Name.Contains('('))) {
     Write-Output ("export " + $_.Name + "='" + $_.Value + "'")
   }
 } | Out-File -Encoding ascii C:\TEMP\refreshenv.sh
