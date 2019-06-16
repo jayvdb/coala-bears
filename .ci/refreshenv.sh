72f8363f8f1aad84009c88ed1d6740c083ff5c27
John Vandenberg <jayvdb@gmail.com>
2019-06-16 09:44:25 +0700

    Use registry

---
+++ .ci/refreshenv.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/bash
+
+echo PATH=$PATH
+
+_SAVEIFS=$IFS
+IFS="
+"
+_DATACMD='reg.exe query "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment"'
+_DATACMD="cat reg.data"
+VARS=$(cat reg.data | sed "s/[[:space:]][[:space:]]\+/\t/g" | tail --lines=+3 | cut -d "	" -f2)
+VALS=$(cat reg.data | sed "s/[[:space:]][[:space:]]\+/\t/g" | tail --lines=+3 | cut -d "	" -f4)
+#echo $VARS
+#echo $VALS
+ARR1=($VARS)
+ARR2=($VALS)
+IFS=_SAVEIFS
+
+COUNT=${#ARR1[*]}
+COUNT2=${#ARR2[*]}
+echo $COUNT $COUNT2
+
+#read -ra ARR <<< "$VARS"
+pad='                             '
+for (( IDX=0 ; IDX<COUNT; IDX++ )) do
+	#echo -n "${ARR1[$IDX]}"
+	PAD=$(printf '%20s' ${ARR1[$IDX]})
+	label=${ARR1[$IDX]}
+	if [ "$label" != "Path" ]; then
+	  continue
+	fi
+	if [ "$label" = "USERNAME" ]; then
+	  continue
+	fi
+	current="${!label}"
+	echo "$label !! $current"
+	if [ -z "$current" ]; then
+	  upper_label=${label^^}
+      echo "$upper_label !! $current"
+	  upper_current="${!upper_label}"
+	  if [ -n "$upper_current" ]; then
+        label=$upper_label
+        current="$upper_current"
+      fi
+    fi
+	raw="${ARR2[$IDX]}"
+	if [ -n "$current" ]; then
+      if [ "$current" = "$raw" ]; then
+        echo $label not changed
+        continue
+      fi
+    fi
+    val="$raw"
+	unixslashes="${val//\\/\/}"
+	printfable="${unixslashes//\%/%%}"
+	echo "$label ^^ $raw"
+
+	if [ -z "$current" ]; then
+	  declare "$label"="$raw"
+	  new="${!label}"
+	  echo "set new $label to $new"
+	elif [ "$current" != "$val" ]; then
+      echo "updating $label was $current"
+      declare "${label}"="$val"
+	  new="${!label}"
+	  echo "updating $label now $new"
+	fi
+	echo "----"
+  done
