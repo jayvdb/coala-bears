fa305d06d5d26d88c8a441e417810d79f9bc7b4a
John Vandenberg <jayvdb@gmail.com>
2019-06-16 16:43:03 +0700

    Use export

--- .ci/gen_refreshenv.ps1
+++ .ci/gen_refreshenv.ps1
@@ -5,6 +5,6 @@ Update-SessionEnvironment
 # Round brackets in variable names cause problems with bash
 Get-ChildItem env:* | %{
   if (!($_.Name -contains '(')) {
-    Write-Output ($_.Name + "='" + $_.Value + "'")
+    Write-Output ("export " + $_.Name + "='" + $_.Value + "'")
   }
 } | Out-File -Encoding utf8 C:\TEMP\refreshenv.sh
