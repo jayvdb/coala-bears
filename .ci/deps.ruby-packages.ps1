3bb6c8419fb0bf8a5d544c2e331f0c79aaae05e3
John Vandenberg <jayvdb@gmail.com>
2019-06-17 17:14:42 +0700

    Skip csvlint gem

--- .ci/deps.ruby-packages.ps1
+++ .ci/deps.ruby-packages.ps1
@@ -1,7 +1,12 @@
 function Install-Gems
 {
   cp -force Gemfile Gemfile.bak
+
+  # Unbuildable on Windows
   sed -i '/sqlint/d' Gemfile
+  # https://github.com/coala/coala-bears/issues/2909
+  sed -i '/csvlint/d' Gemfile
+
   bundle install
   mv -force Gemfile.bak Gemfile
 }
