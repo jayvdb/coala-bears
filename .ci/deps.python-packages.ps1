3143c50dc9298ad7bcd8f883f3a568e494c800b2
John Vandenberg <jayvdb@gmail.com>
2019-06-25 22:31:10 +0700

    Fix ~=

--- .ci/deps.python-packages.ps1
+++ .ci/deps.python-packages.ps1
@@ -161,7 +161,7 @@ function Install-Test-Packages {
         Freeze-Pip-Constraints
 
         # tox 3.13 uses pluggy 0.12.0 which is incompatible with a pytest 3.6.4
-        Install-Pip-Requirement 'tox=~3.12.0 tox-backticks'
+        Install-Pip-Requirement 'tox~=3.12.0 tox-backticks'
     }
 }
 
