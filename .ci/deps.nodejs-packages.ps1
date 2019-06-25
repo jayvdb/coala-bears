a445d2830fb5eb40f3095c7d7cbc44724fc821d5
John Vandenberg <jayvdb@gmail.com>
2019-06-25 20:13:06 +0700

    Ignore errors in npm install

--- .ci/deps.nodejs-packages.ps1
+++ .ci/deps.nodejs-packages.ps1
@@ -15,7 +15,14 @@ function Install-Node-Packages {
     sed -i '/textlint-plugin-asciidoc-loose/d' package.json
 
     # If gyp fails, use npm config python to help locate Python 2.7
+
+    $old_EAP = $ErrorActionPreference
+    $ErrorActionPreference = 'Continue'
+
     npm install
+
+    $ErrorActionPreference = $old_EAP
+
     mv -force package.json.bak package.json
 
     npm config set reporter default
