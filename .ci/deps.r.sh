86137dc5d6c6c4d3458b08295d893e111b5010e3
John Vandenberg <jayvdb@gmail.com>
2019-06-28 21:27:00 +0700

    Use old Rprofile

--- .ci/deps.r.sh
+++ .ci/deps.r.sh
@@ -5,6 +5,6 @@ set -x
 
 # R commands
 echo '.libPaths( c( "'"$R_LIB_USER"'", .libPaths()) )' >> ~/.Rprofile
-#echo 'options(repos=structure(c(CRAN="http://cran.rstudio.com")))' >> .Rprofile
+echo 'options(repos=structure(c(CRAN="http://cran.rstudio.com")))' >> .Rprofile
 #R -q -e 'install.packages("lintr")'
 #R -q -e 'install.packages("formatR")'
