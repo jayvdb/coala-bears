56f88635c77c38ad38f0fad114075657320d7778
John Vandenberg <jayvdb@gmail.com>
2019-07-16 17:49:09 +0700

    Remove TRAVIS_LANGUAGE

--- .ci/deps.gem.sh
+++
@@ -1,3 +0,0 @@
-if [ -z "$TRAVIS_LANGUAGE" ]; then
-  bundle install --path=vendor/bundle --binstubs=vendor/bin --jobs=8 --retry=3
-fi
--- .ci/deps.node_js.sh
+++ .ci/deps.node_js.sh
@@ -2,12 +2,6 @@
 
 set -e -x
 
-# NPM commands
 source .ci/deps.alex.sh
-
-if [ -z "$TRAVIS_LANGUAGE" ]; then
-  npm install
-  npm list --depth=0
-fi
 
 source .ci/deps.elm.sh
