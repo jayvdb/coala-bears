6458f72b0bc7f4f46a6c6a603f31ce77df429638
John Vandenberg <jayvdb@gmail.com>
2019-07-16 21:51:12 +0700

    deps.r.sh: Remove custom path

--- .ci/deps.r.sh
+++ .ci/deps.r.sh
@@ -3,5 +3,4 @@
 set -e
 set -x
 
-echo '.libPaths( c( "/usr/local/lib/R/site-library", .libPaths()) )' >> ~/.Rprofile
 echo 'options(repos=structure(c(CRAN="http://cran.rstudio.com")))' >> .Rprofile
