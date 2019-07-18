e2f7b1e9aee07705c0a829edba21ce4bfc8c3c2f
John Vandenberg <jayvdb@gmail.com>
2019-07-18 17:53:17 +0700

    pyenv more defensive

--- .ci/deps.pyenv.sh
+++ .ci/deps.pyenv.sh
@@ -6,9 +6,13 @@ if [ -z "$(which pyenv)" ]; then
   # This is only useful if sources
   export PYENV_ROOT="$HOME/.pyenv";
   export PATH="$PYENV_ROOT/bin:$PATH";
-  if [ ! -d "$PYENV_ROOT" ]; then
-    git clone https://github.com/pyenv/pyenv.git ~/.pyenv;
-    if [ -d ~/local ]; then
+  if [ ! -f "$PYENV_ROOT/bin/pyenv" ]; then
+    if [ -d "$PYENV_ROOT" ]; then
+      rm -rf "$PYENV_ROOT"
+    fi
+    git clone https://github.com/pyenv/pyenv.git $PYENV_ROOT;
+    if [ -d ~/local/bin ]; then
+      rm ~/local/bin/pyenv
       (cd ~/local/bin && ln -s $PYENV_ROOT/bin/pyenv .)
     fi
   fi
