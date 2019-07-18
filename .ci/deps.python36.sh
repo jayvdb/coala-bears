92616eb50bd15526f27ac010663e14e86167d606
John Vandenberg <jayvdb@gmail.com>
2019-07-18 15:18:38 +0700

    Avoid multi commands

--- .ci/deps.python36.sh
+++ .ci/deps.python36.sh
@@ -5,10 +5,7 @@ set -e -x
 PYTHON36_VERSION=$(pyenv versions --bare | fgrep '3.6' --max-count 1 || true)
 
 if [ -z "$PYTHON36_VERSION" ]; then
-  git clone https://github.com/s1341/pyenv-alias.git \
-    $(pyenv root)/plugins/pyenv-alias;
-
-  export PYTHON36_VERSION=3.6.3
+  PYTHON36_VERSION=3.6.3
 
   pyenv install "$PYTHON36_VERSION";
 fi
