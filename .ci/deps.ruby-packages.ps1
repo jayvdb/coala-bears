edbc4801e354cc737973a33ed50f31ce507a11fb
John Vandenberg <jayvdb@gmail.com>
2019-07-19 11:28:27 +0700

    Gemfile switcheroo

--- .ci/deps.ruby-packages.ps1
+++ .ci/deps.ruby-packages.ps1
@@ -1,15 +1,18 @@
 function Install-Gemfile {
-    cp -force Gemfile Gemfile.bak
+    cp -force Gemfile Gemfile.orig
+    cp -force Gemfile Gemfile.win
 
     # Unbuildable on Windows
-    sed -i '/sqlint/d' Gemfile
+    sed -i '/sqlint/d' Gemfile.win
     # https://github.com/coala/coala-bears/issues/2909
-    sed -i '/csvlint/d' Gemfile
+    sed -i '/csvlint/d' Gemfile.win
 
     # pusher-client 0.4.0 doesnt depend on json, which requires
     # a compiler and the GMP library
     Write-Output 'gem "pusher-client", "~>0.4.0", require: false' |
-        Out-File -FilePath Gemfile -Append -Encoding ascii
+        Out-File -FilePath Gemfile.win -Append -Encoding ascii
+
+    cp -force Gemfile.win Gemfile
 
     # The build crawls if DevKit is included in the PATH
     $old_PATH = $env:PATH
@@ -19,7 +22,7 @@ function Install-Gemfile {
 
     $env:PATH = $old_PATH
 
-    mv -force Gemfile.bak Gemfile
+    mv -force Gemfile.orig Gemfile
 }
 
 function Invoke-ExtraInstallation {
