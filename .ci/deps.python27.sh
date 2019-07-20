a694e7656b8d81fe214580b11201156c7ea28c38
John Vandenberg <jayvdb@gmail.com>
2019-07-20 18:55:00 +0700

    Register system 2.7

--- .ci/deps.python27.sh
+++ .ci/deps.python27.sh
@@ -2,5 +2,16 @@
 
 set -e -x
 
+PYTHON27_BIN="$(which python2.7 || true)"
+if [ -n "$PYTHON27_BIN" ]; then
+  if [ ! -d "$PYENV_ROOT/plugins/pyenv-register" ]; then
+    # https://github.com/doloopwhile/pyenv-register/pull/3
+    git clone https://github.com/garyp/pyenv-register.git \
+      "$PYENV_ROOT/plugins/pyenv-register"
+  fi
+
+  pyenv register -f "$PYTHON27_BIN" || true
+fi
+
 pyenv global 2.7 $(pyenv versions --bare | fgrep '3.6' --max-count 1)
 hash -r
